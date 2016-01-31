10c11ff992c0fc4f9b0a28c3b4efcef262d95539
slwk <sylwester.balcerek@gmail.com>
2016-01-31 17:11:07 +0100

    Added proxy check

--- src/main/java/sb/quickchecks/proxy/ProxyCheck.java
+++ src/main/java/sb/quickchecks/proxy/ProxyCheck.java
@@ -5,10 +5,62 @@
  */
 package sb.quickchecks.proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  *
  * @author slwk
  */
 public class ProxyCheck {
+ 
+    interface SampleInterface {
+        public void doSmth();
+    }
     
+    static class ProxyCheckHandler implements InvocationHandler {
+        
+        private SampleInterface original;
+
+        public ProxyCheckHandler(SampleInterface original) {
+            this.original = original;
+        }
+        
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("Before method invoke");
+            Object returned = method.invoke(original, args);
+            System.out.println("After method invoked");
+            return returned;
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        SampleInterface sampleInterface = new SampleInterface() {
+
+            @Override
+            public void doSmth() {
+                System.out.println("Inside doSmth method");
+            }
+        };
+        
+        SampleInterface sampleInterfaceWithoutProxy = new SampleInterface() {
+
+            @Override
+            public void doSmth() {
+                System.out.println("Inside doSmth method without proxy");
+            }
+        };
+        
+        InvocationHandler handler = new ProxyCheckHandler(sampleInterface);
+        
+        SampleInterface proxy = (SampleInterface) Proxy.newProxyInstance(SampleInterface.class.getClassLoader(), new Class[] {SampleInterface.class}, handler);
+        
+        proxy.doSmth();
+        
+        sampleInterfaceWithoutProxy.doSmth();
+    }
 }
