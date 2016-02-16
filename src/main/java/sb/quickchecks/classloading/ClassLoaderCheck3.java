100f00a9210f4fb4e2c00ac1a8d3d4cc0eb88f6a
slwk <sylwester.balcerek@gmail.com>
2016-02-16 22:35:57 +0100

    Custom class loader check refactored

--- src/main/java/sb/quickchecks/classloading/ClassLoaderCheck3.java
+++ src/main/java/sb/quickchecks/classloading/ClassLoaderCheck3.java
@@ -7,10 +7,7 @@ package sb.quickchecks.classloading;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.util.converter.ShortStringConverter;
-import sb.quickchecks.classloading.model.DummyClass;
 
 /**
  *
@@ -18,113 +15,94 @@ import sb.quickchecks.classloading.model.DummyClass;
  */
 public class ClassLoaderCheck3 {
 
+    private static final String classNameToLoad = "sb.java8.TestStreams";
+//    private static final String classNameToLoad = "sb.quickchecks.classloading.model.DummyClass";
+    
+    private static final String pathToClassFile ="C:\\Users\\slwk\\Documents\\NetBeansProjects\\Java8Testing\\target\\classes\\sb\\java8\\TestStreams.class";
+//    private static final String pathToClassFile ="C:\\Users\\slwk\\Documents\\NetBeansProjects\\QuickChecks\\target\\classes\\sb\\quickchecks\\classloading\\model\\DummyClass.class";
+
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        
-//        MyClassLoader classLoader = new MyClassLoader(ShortStringConverter.class.getClassLoader());
-        MyClassLoader classLoader = new MyClassLoader();
-        
-//        MultiClassLoader classLoader = new JarClassLoader();
-        
-//        MultiClassLoader classLoader = new MultiClassLoader();
+
+        MyClassLoader classLoader = new MyClassLoader(pathToClassFile, ShortStringConverter.class.getClassLoader());
+//        MyClassLoader classLoader = new MyClassLoader();
+
         System.out.println(ShortStringConverter.class.getClassLoader());
         System.out.println(classLoader);
-        Class d = classLoader.findClass("sb.quickchecks.classloading.model.DummyClass");
-//        Class<DummyClass> d = classLoader.loadClass("sb.quickchecks.classloading.model.DummyClass",true);
-//        DummyClass dc =  d.newInstance();
-//        System.out.println(dc.getName());
-        System.out.println(d.getClassLoader());
+
+        Class d = classLoader.loadClass(classNameToLoad);
+//        Class d = classLoader.findClass(classNameToLoad);
+
+        System.out.println("Class loader of loaded class: " + d.getClassLoader());
+        System.out.println("Loaded class name: " + d.getCanonicalName());
+        System.out.println(d.newInstance().toString());
 
     }
 
-   
 }
 
 class MyClassLoader extends ClassLoader {
+    
+    private final String pathToClassFile;
 
-        public MyClassLoader() {
-            
-        }
-        
-//        public MyClassLoader(ClassLoader parent) {
-//            super(parent);
-//        }
-
-    @Override
-    protected Class<DummyClass> findClass(String name) throws ClassNotFoundException {
-      
-
-        
-            
-            byte[] classBytes = null;
-            try {
-                classBytes = readClassBytes();
-            } catch (IOException ex) {
-                Logger.getLogger(ClassLoaderCheck3.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-//                        try {
-//			super.findSystemClass(name);
-//			
-//			
-//		} catch (ClassNotFoundException e) {
-//			
-//		}
-            
-            Class c =  defineClass(name, classBytes, 0, classBytes.length);
-            resolveClass(c);
-            return c;
-           
-        }
-        
-        private byte[] readClassBytes()  throws IOException{
-            FileInputStream fis = new FileInputStream("C:\\Users\\slwk\\Documents\\NetBeansProjects\\QuickChecks\\target\\classes\\sb\\quickchecks\\classloading\\model\\DummyClass.class");
-            
-            byte[] bytes = new byte[fis.available()];
-            fis.read(bytes);
-            
-//            fis.close();
-            
-            return bytes;
-        }
-
-    @Override
-    protected Class<DummyClass> loadClass(String className, boolean resolve) throws ClassNotFoundException {
-      
-        
-        
-
-		Class result;
-		byte[] classBytes = null;
-
-
-
-
-
-            try {
-                // ----- Try to load it from preferred source
-                // Note loadClassBytes() is an abstract method
-                classBytes = readClassBytes();
-            } catch (IOException ex) {
-                Logger.getLogger(MyClassLoader.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            try {
-			result = super.findSystemClass(className);
-			
-			return result;
-		} catch (ClassNotFoundException e) {
-			
-		}
-
-		// ----- Define it (parse the class file)
-		result = defineClass(className, classBytes, 0, classBytes.length);
-
-
-
-			resolveClass(result);
-
-
-
-		return result;
-	}
+    public MyClassLoader(String pathToClassFile) {
+        this.pathToClassFile=pathToClassFile;
     }
+
+    public MyClassLoader(String pathToClassFile, ClassLoader parent) {
+        super(parent);
+        this.pathToClassFile=pathToClassFile;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        byte[] classBytes = null;
+        try {
+            classBytes = readClassBytes();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            super.findSystemClass(name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        Class c = defineClass(name, classBytes, 0, classBytes.length);
+        resolveClass(c);
+        return c;
+    }
+
+    private byte[] readClassBytes() throws IOException {
+        FileInputStream fis = new FileInputStream(pathToClassFile);
+        byte[] bytes = new byte[fis.available()];
+        fis.read(bytes);
+        fis.close();
+        return bytes;
+    }
+
+//    @Override
+//    protected Class<?> loadClass(String className, boolean resolve) throws ClassNotFoundException {
+//
+//        Class result;
+//        byte[] classBytes = null;
+//
+//        try {
+//            classBytes = readClassBytes();
+//        } catch (IOException ex) {
+//            Logger.getLogger(MyClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        try {
+//            result = super.findSystemClass(className);
+//            return result;
+//        } catch (ClassNotFoundException e) {
+//
+//        }
+//
+//        result = defineClass(className, classBytes, 0, classBytes.length);
+//        resolveClass(result);
+//        return result;
+//    }
+}
