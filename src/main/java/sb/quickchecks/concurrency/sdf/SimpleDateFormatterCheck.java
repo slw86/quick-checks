a12e4f62ee4e735c50c4b392b5846230562f785c
slwk <sylwester.balcerek@gmail.com>
2016-05-06 23:36:37 +0200

    SimpleDateFormatterCheck refactored

--- src/main/java/sb/quickchecks/concurrency/sdf/SimpleDateFormatterCheck.java
+++ src/main/java/sb/quickchecks/concurrency/sdf/SimpleDateFormatterCheck.java
@@ -1,7 +1,9 @@
 package sb.quickchecks.concurrency.sdf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,27 +12,33 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
-/**
- * Created by sbalcerek on 2016-05-05.
- */
+
 public class SimpleDateFormatterCheck {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDateFormatterCheck.class);
 
     public static final int THREADS_NUMBER = 10;
     public static final int RANGE = 1000;
-
-    public static final Random rand = new Random();
-
-
     public static final String PATTERN = "dd-MM-YYYY hh:mm:ss";
-    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
+
+
 
     public static void main(String[] args) {
-
 
         ExecutorService executorService = Executors.newFixedThreadPool(THREADS_NUMBER);
 
 
-        Map<String, Date> stringDateMap = initDates(RANGE);
+        Map<String, Date> stringDateMap = getMapFilledWithRandomDates(RANGE);
+
+        runMultithreadedFormatting(executorService, stringDateMap);
+
+
+        executorService.shutdown();
+
+    }
+
+    private static void runMultithreadedFormatting(ExecutorService executorService, Map<String, Date> stringDateMap) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
 
         stringDateMap.forEach(
                 (s, date) -> executorService.submit(
@@ -39,25 +47,22 @@ public class SimpleDateFormatterCheck {
                             String formatted = simpleDateFormat.format(date);
 
                             if(!formatted.equals(s)) {
-                                System.out.println("---- Preformatted date: "+s+" Formatted: "+formatted+"  Date: "+date);
+                                LOGGER.error("Preformatted date: {} Formatted: {}  Date: {}", s, formatted, date);
                             }
                         })
         );
-
-
-        executorService.shutdown();
-
     }
 
-    static Map<String, Date> initDates(int range) {
+    static Map<String, Date> getMapFilledWithRandomDates(int range) {
 
+        Random rand = new Random();
 
         Map<String, Date> dates = new HashMap<>();
-        SimpleDateFormat sdf = new SimpleDateFormat(PATTERN);
+        SimpleDateFormat sdf = new SimpleDateFormat(PATTERN); //this sdf is only accessible by single thread
 
         IntStream.range(0, range).forEach(
                 v -> {
-                    Date random = getRandomDate();
+                    Date random = getRandomDate(rand);
                     dates.put(sdf.format(random), random);
                 }
         );
@@ -66,7 +71,7 @@ public class SimpleDateFormatterCheck {
         return dates;
     }
 
-    static Date getRandomDate() {
+    static Date getRandomDate(Random rand) {
         return new Date(rand.nextInt(199), rand.nextInt(12), rand.nextInt(31), rand.nextInt(12), rand.nextInt(60), rand.nextInt(60));
     }
 }
